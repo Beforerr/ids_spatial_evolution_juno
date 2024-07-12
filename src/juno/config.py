34c8ee05811cdd9f9c336d66cdd92e1798660952
Beforerr <zzj956959688@gmail.com>
2024-07-11 17:51:35 -0700

    add back Wind Config

--- src/juno/config.py
+++ src/juno/config.py
@@ -4,10 +4,11 @@ import polars as pl
 from datetime import timedelta, datetime
 
 from beforerr.project import datadir
-from space_analysis.meta import MagDataset, PlasmaDataset
+from space_analysis.meta import PlasmaDataset
 from discontinuitypy.datasets import IDsDataset
 from discontinuitypy.config import IDsConfig as IDsConfigBase
 from discontinuitypy.config import SpeasyIDsConfig as SpeasyIDsConfigBase
+from discontinuitypy.mission import WindConfigBase, ThemisConfigBase, StereoConfigBase
 
 from .juno import get_mag_paths
 from math import ceil
@@ -45,22 +46,16 @@ class SpeasyIDsConfig(IDsConfig, SpeasyIDsConfigBase):
     pass
 
 
-def create_juno_config(name="JNO"):
-    plasma_df_path = datadir() / "03_primary/JNO_STATE_ts_3600s.parquet"
-    plasma_data = pl.scan_parquet(plasma_df_path).sort("time")
-    plasma_meta = PlasmaDataset(
-        density_col="plasma_density", velocity_cols=["v_x", "v_y", "v_z"]
-    )
-
-    return IDsConfig(
-        name=name,
-        plasma_data=plasma_data,
-        plasma_meta=plasma_meta,
-    )
+JNO_PLASMA_DF_PATH = datadir() / "03_primary/JNO_STATE_ts_3600s.parquet"
 
 
 class JunoConfig(IDsConfig):
     _sparse_num = 10
+    name = "JNO"
+    plasma_data: pl.DataFrame = pl.scan_parquet(JNO_PLASMA_DF_PATH).sort("time")
+    plasma_meta = PlasmaDataset(
+        density_col="plasma_density", velocity_cols=["v_x", "v_y", "v_z"]
+    )
 
     def model_post_init(self, __context):
         self.data = self.mag_df
@@ -99,41 +94,15 @@ class JunoConfig(IDsConfig):
             )
 
 
-# class WindConfig(WindMeta, SpeasyIDsConfig):
-#     provider: str = "archive/local"
-#     mag_meta: MagDataset = wind_mag_h4_rtn_meta
-#     plasma_meta: PlasmaDataset = wind_plasma_k0_swe_meta
-#     split: int = 32
+class WindConfig(WindConfigBase, SpeasyIDsConfig):
+    provider: str = "archive/local"
+    # mag_meta: MagDataset = wind_mag_h4_rtn_meta
+    # plasma_meta: PlasmaDataset = wind_plasma_k0_swe_meta
 
 
-class StereoConfig(IDsConfig):
+class StereoConfig(StereoConfigBase, SpeasyIDsConfig):
     ts: timedelta = timedelta(seconds=1)
 
-    mag_meta: MagDataset = MagDataset(
-        dataset="STA_L1_MAG_RTN",
-        parameters=["BFIELD"],
-    )
 
-    plasma_meta: PlasmaDataset = PlasmaDataset(
-        dataset="STA_L2_PLA_1DMAX_1MIN",
-        parameters=[
-            "proton_number_density",
-            # "V_GSM",
-            "proton_temperature",
-        ],  # TODO: is GSM close to RTN coordinates???
-    )
-
-
-class THEMISConfig(IDsConfig):
-    mag_meta: MagDataset = MagDataset(
-        dataset="THB_L2_FGM",
-        parameters=["thb_fgl_gse"],
-    )
-
-    plasma_meta: PlasmaDataset = PlasmaDataset(
-        dataset="THB_L2_MOM",
-        parameters=[
-            "thb_peim_densityQ",
-            "thb_peim_velocity_gseQ",
-        ],
-    )
+class THEMISConfig(ThemisConfigBase, SpeasyIDsConfig):
+    pass
