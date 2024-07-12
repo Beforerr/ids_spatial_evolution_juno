ff145d4d3fd70fb15482ef4daeb626a68b4ffa6f
Beforerr <zzj956959688@gmail.com>
2024-07-12 06:02:24 -0700

    reorder notebooks

--- src/juno/config.py
+++ src/juno/config.py
@@ -1,13 +1,14 @@
 from pydantic import Field
+from pydantic import BaseModel, ConfigDict
 import polars as pl
 
 from datetime import timedelta, datetime
+from pathlib import Path
 
 from beforerr.project import datadir
 from space_analysis.meta import PlasmaDataset
 from discontinuitypy.datasets import IDsDataset
-from discontinuitypy.config import IDsConfig as IDsConfigBase
-from discontinuitypy.config import SpeasyIDsConfig as SpeasyIDsConfigBase
+from discontinuitypy.config import SpeasyIDsConfig
 from discontinuitypy.mission import WindConfigBase, ThemisConfigBase, StereoConfigBase
 
 from .juno import get_mag_paths
@@ -24,36 +25,31 @@ def split_list(lst, n):
     return list(_split_list(lst, n))
 
 
-tau: timedelta = timedelta(seconds=60)
-timerange: list[datetime] = Field(["2011-08-25", "2016-06-30"], validate_default=True)
+DEFAULT_TAU = timedelta(seconds=60)
+DEFAULT_TS = timedelta(seconds=1)
+DEFAULT_TIMERANGE = Field(["2011-08-25", "2016-06-30"], validate_default=True)
+DEFAULT_TEST_TIMERANGE = Field(["2012-01-01", "2012-01-02"], validate_default=True)
+JNO_PLASMA_DF_PATH = datadir() / "03_primary/JNO_STATE_ts_3600s.parquet"
 
 
-class IDsConfig(IDsConfigBase):
-    tau: timedelta = timedelta(seconds=60)
-    # ts: timedelta = timedelta(seconds=1)
-    timerange: list[datetime] = timerange
+class Config(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="allow")
+
+    tau: timedelta = DEFAULT_TAU
+    ts: timedelta = DEFAULT_TS
+    timerange: list[datetime] = DEFAULT_TIMERANGE
+
+    data_dir: Path = datadir() / "05_reporting"
 
     split: int = 16
     test: bool = False
 
-    def model_post_init(self, __context):
-        if self.test:
-            self.timerange = [datetime(2012, 1, 1), datetime(2012, 1, 2)]
-        super().model_post_init(__context)
 
-
-class SpeasyIDsConfig(IDsConfig, SpeasyIDsConfigBase):
-    pass
-
-
-JNO_PLASMA_DF_PATH = datadir() / "03_primary/JNO_STATE_ts_3600s.parquet"
-
-
-class JunoConfig(IDsConfig):
+class JunoConfig(Config, IDsDataset):
     _sparse_num = 10
-    name = "JNO"
+    name: str = "JNO"
     plasma_data: pl.DataFrame = pl.scan_parquet(JNO_PLASMA_DF_PATH).sort("time")
-    plasma_meta = PlasmaDataset(
+    plasma_meta: PlasmaDataset = PlasmaDataset(
         density_col="plasma_density", velocity_cols=["v_x", "v_y", "v_z"]
     )
 
@@ -94,13 +90,13 @@ class JunoConfig(IDsConfig):
             )
 
 
-class WindConfig(WindConfigBase, SpeasyIDsConfig):
+class WindConfig(Config, WindConfigBase, SpeasyIDsConfig):
     provider: str = "archive/local"
     # mag_meta: MagDataset = wind_mag_h4_rtn_meta
     # plasma_meta: PlasmaDataset = wind_plasma_k0_swe_meta
 
 
-class StereoConfig(StereoConfigBase, SpeasyIDsConfig):
+class StereoConfig(Config, StereoConfigBase, SpeasyIDsConfig):
     ts: timedelta = timedelta(seconds=1)
 
 
