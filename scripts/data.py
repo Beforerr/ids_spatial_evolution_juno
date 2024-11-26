c569b95e2268342ccd34b35632f2198ab9a4720c
Beforerr <zzj956959688@gmail.com>
2024-11-25 16:12:39 -0800

    fix: update data scripts

--- scripts/data.py
+++ scripts/data.py
@@ -1,4 +1,24 @@
-#| hide
-from juno.config import JunoConfig
+# %%
+from rich import print
+from juno.config import JunoConfig, WindConfig, StereoConfig
 
-JunoConfig().produce_or_load()
+from speasy.core.requests_scheduling.request_dispatch import init_archive
+
+init_archive()
+
+# %%
+for c in [JunoConfig, WindConfig, StereoConfig]:
+    c().produce_or_load()
+
+# Different time windows
+taus = range(60, 10, -10)
+
+for tau in taus:
+    JunoConfig(tau=tau).produce_or_load()
+# %%
+# Test
+w_conf = WindConfig(test=True)
+w_conf.get_data()
+w_conf.plasma_data
+print(w_conf)
+# %%
--- src/juno/config.py
+++ src/juno/config.py
@@ -1,4 +1,3 @@
-from pydantic import Field
 from pydantic import BaseModel, ConfigDict
 import polars as pl
 
@@ -6,12 +5,13 @@ from datetime import timedelta, datetime
 from pathlib import Path
 
 from beforerr.project import datadir
+from space_analysis.core import MagVariable
 from space_analysis.meta import PlasmaDataset
 from discontinuitypy.config import SpeasyIDsConfig, IDsConfig
 from discontinuitypy.mission import WindConfigBase, ThemisConfigBase, StereoConfigBase
 
 from .juno import get_mag_data
-from ..juno import DEFAULT_TIMERANGE, DEFAULT_TEST_TIMERANGE
+from . import DEFAULT_TIMERANGE, DEFAULT_TEST_TIMERANGE
 from math import ceil
 
 
@@ -27,8 +27,6 @@ def split_list(lst, n):
 
 DEFAULT_TAU = timedelta(seconds=60)
 DEFAULT_TS = timedelta(seconds=1)
-DEFAULT_TIMERANGE_F = Field(DEFAULT_TIMERANGE, validate_default=True)
-DEFAULT_TEST_TIMERANGE_F = Field(DEFAULT_TEST_TIMERANGE, validate_default=True)
 JNO_PLASMA_DF_PATH = datadir() / "03_primary/JNO_STATE_ts_3600s.parquet"
 
 
@@ -36,21 +34,41 @@ class Config(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True, extra="allow")
 
     tau: timedelta = DEFAULT_TAU
-    timerange: list[datetime] = DEFAULT_TIMERANGE_F
-
-    data_dir: Path = datadir() / "05_reporting"
+    timerange: list[datetime] = DEFAULT_TIMERANGE
 
     split: int = 16
     test: bool = False
+
+    file_path: Path = datadir() / "05_reporting"
+
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+        self.detect_kwargs = {"tau": self.tau}
+        if self.test:
+            self.timerange = DEFAULT_TEST_TIMERANGE
+            self.split = 1
+
+
+# %%
+def juno_plasma_data(source=JNO_PLASMA_DF_PATH):
+    v_cols = ["v_x", "v_y", "v_z"]
+    B_background_cols = ["B_background_" + c for c in "xyz"]
+    B_model_cols = ["model_b_" + c for c in "rtn"]
+    return (
+        pl.scan_parquet(source)
+        .with_columns(V=pl.concat_list(*v_cols).list.to_array(3))
+        .drop(v_cols + B_background_cols + B_model_cols + ["plasma_speed"])
+        .sort("time")
+    )
 
 
 class JunoConfig(Config, IDsConfig):
     name: str = "JNO"
     ts: timedelta = DEFAULT_TS
-    plasma_data: pl.DataFrame = pl.scan_parquet(JNO_PLASMA_DF_PATH).sort("time")
+    plasma_data: pl.DataFrame = juno_plasma_data()
     plasma_meta: PlasmaDataset = PlasmaDataset(
         density_col="plasma_density",
-        velocity_cols=["v_x", "v_y", "v_z"],
+        velocity_cols="V",
         temperature_col="plasma_temperature",
     )
 
@@ -64,10 +82,21 @@ class JunoConfig(Config, IDsConfig):
         return get_mag_data(self.timerange, freq)
 
 
+wi_mfi_h4_brtn = MagVariable(
+    dataset="WI_H4-RTN_MFI",
+    parameter=["BRTN"],
+    links=["https://cdaweb.gsfc.nasa.gov/misc/NotesW.html#WI_H4-RTN_MFI"],
+)
+
+wi_k0_swe_ds = PlasmaDataset(
+    dataset="WI_K0_SWE", parameters=["Np", "V_GSE", "THERMAL_SPD"]
+)
+
+
 class WindConfig(Config, WindConfigBase, SpeasyIDsConfig):
     provider: str = "archive/local"
-    # mag_meta: MagDataset = wind_mag_h4_rtn_meta
-    # plasma_meta: PlasmaDataset = wind_plasma_k0_swe_meta
+    mag_meta: MagVariable = wi_mfi_h4_brtn
+    plasma_meta: PlasmaDataset = wi_k0_swe_ds
 
 
 class StereoConfig(Config, StereoConfigBase, SpeasyIDsConfig):
@@ -76,3 +105,6 @@ class StereoConfig(Config, StereoConfigBase, SpeasyIDsConfig):
 
 class THEMISConfig(ThemisConfigBase, SpeasyIDsConfig):
     pass
+
+
+# %%
